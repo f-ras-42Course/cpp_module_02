723051b8b21ebdd6c5e99929d95e9c5d6066e0fa
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-05-24 16:12:59 +0200

    init funcitons

--- ex00/include/Fixed.hpp
+++ ex00/include/Fixed.hpp
@@ -6,19 +6,23 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/20 16:33:42 by fras          #+#    #+#                 */
-/*   Updated: 2024/05/21 14:44:37 by fras          ########   odam.nl         */
+/*   Updated: 2024/05/24 15:19:15 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
 
 class Fixed
 {
 	private:
-		int fixed_point_value_;
+		int fixed_point_number_value_;
 		static const int fractional_bits_ = 8;			
 	public:
 		Fixed();
-		Fixed(const Fixed& fixed);
+		Fixed(const Fixed& other);
 		// Fixed(int i);
 		~Fixed();
-		Fixed& operator = (const Fixed& fixed); 
+		Fixed& operator=(const Fixed& other);
+		int	getRawBits() const;
+		void setRawBits(const int raw);
 };
--- ex00/src/Fixed.cpp
+++ ex00/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/20 16:33:33 by fras          #+#    #+#                 */
-/*   Updated: 2024/05/21 13:10:48 by fras          ########   odam.nl         */
+/*   Updated: 2024/05/24 16:12:46 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,35 @@
 
 Fixed::Fixed()
 {
+	std::cout << "Default constructor Called\n";
 }
-Fixed::Fixed(const Fixed& fixed)
+Fixed::Fixed(const Fixed& other)
 {
-
+	std::cout << "Copy constructor called\n";
+	this->fixed_point_number_value_ = other.fixed_point_number_value_;
 }
 
 Fixed::~Fixed()
 {
+	std::cout << "Destructor called\n";
+}
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+	std::cout << "Copy assignment operator called\n";
+	if (this == &other)
+		return *this;
+	
+	this->fixed_point_number_value_ = other.fixed_point_number_value_;
+	return *this;
+}
+
+int	Fixed::getRawBits() const
+{
+	return (fixed_point_number_value_);
+}
+
+void Fixed::setRawBits(const int raw)
+{
+	fixed_point_number_value_ = raw;	
 }

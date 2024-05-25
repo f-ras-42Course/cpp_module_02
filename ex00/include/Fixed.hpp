1a78ea4ad8102592d6892dbd7cb855d4b567933d
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-05-26 00:38:33 +0200

    refactor var name

--- ex00/include/Fixed.hpp
+++ ex00/include/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/20 16:33:42 by fras          #+#    #+#                 */
-/*   Updated: 2024/05/24 16:25:09 by fras          ########   odam.nl         */
+/*   Updated: 2024/05/26 00:33:49 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 class Fixed
 {
 	private:
-		int fixed_point_number_value_;
+		int fixed_point_value_;
 		static const int fractional_bits_ = 8;			
 	public:
 		Fixed();
--- ex00/src/Fixed.cpp
+++ ex00/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/20 16:33:33 by fras          #+#    #+#                 */
-/*   Updated: 2024/05/24 18:08:27 by fras          ########   odam.nl         */
+/*   Updated: 2024/05/26 00:33:49 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 Fixed::Fixed()
 {
 	std::cout << "Default constructor called\n";
-	fixed_point_number_value_ = 0;
+	fixed_point_value_ = 0;
 }
 Fixed::Fixed(const Fixed& other)
 {
@@ -34,18 +34,18 @@ Fixed& Fixed::operator=(const Fixed& other)
 	if (this == &other)
 		return *this;
 	
-	this->fixed_point_number_value_ = other.getRawBits();
+	this->fixed_point_value_ = other.getRawBits();
 	return *this;
 }
 
 int	Fixed::getRawBits() const
 {
 	std::cout << "getRawBits member function called\n";
-	return (fixed_point_number_value_);
+	return (fixed_point_value_);
 }
 
 void Fixed::setRawBits(const int raw)
 {
 	std::cout << "setRawBits member function called\n";
-	fixed_point_number_value_ = raw;	
+	fixed_point_value_ = raw;	
 }

926f36c4260cfc1abca62b978ebe43a84ff9c17a
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-05-26 05:31:43 +0200

    remove test prints in main

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/25 15:52:48 by fras          #+#    #+#                 */
-/*   Updated: 2024/05/26 00:31:35 by fras          ########   odam.nl         */
+/*   Updated: 2024/05/26 05:29:57 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,6 @@ int main()
 	std::cout << "float " << x << std::endl;
 	std::cout << "f is (int)" << f.toInt() << std::endl;
 	std::cout << "f is (float)" << f.toFloat() << std::endl;
-	
-
-	float a = 1;
-	std::cout << a << std::endl;
-	for (size_t i = 0; i < 8; i++)
-	{
-		a /= 2;
-		std::cout << a << std::endl;
-	}
-
-	std::cout << "\n\n";
-	a = 16777215;
-	std::cout << a << std::endl;
-	for (size_t i = 0; i < 32; i++)
-	{
-		a /= 2;
-		std::cout << a << std::endl;
-	}	
 	// Fixed a;
 	// Fixed const b( 10 );
 	// Fixed const c( 42.42f );
